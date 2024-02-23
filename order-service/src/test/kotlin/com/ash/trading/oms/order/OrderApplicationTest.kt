145550d05d0a30834349960d90a36ccaa7743117
Ash French <ash.french1@gmail.com>
2024-02-23 15:01:06 +0000

    update gradle dependency resolution management

---
+++ order-service/src/test/kotlin/com/ash/trading/oms/order/OrderApplicationTest.kt
@@ -0,0 +1,15 @@
+package com.ash.trading.oms.order
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderApplicationTest {
+
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,15 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("swagger", "org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+        }
+    }
+}
+
 rootProject.name = "trade-oms"
 include("data-model")
 include("trade-service")
