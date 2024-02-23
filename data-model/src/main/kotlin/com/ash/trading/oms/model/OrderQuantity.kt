df82b40ff2f6e5e4a15bb967125b383568c00d8a
Ash French <ash.french1@gmail.com>
2024-02-23 15:22:54 +0000

    Update quantities

--- data-model/src/main/kotlin/com/ash/trading/oms/model/OrderQuantity.kt
+++ data-model/src/main/kotlin/com/ash/trading/oms/model/OrderQuantity.kt
@@ -3,18 +3,25 @@ package com.ash.trading.oms.model
 import java.math.BigDecimal
 
 data class OrderQuantity(
-    val totalQuantity: BigDecimal,
-    val openQuantity: BigDecimal,
-    val executedQuantity: BigDecimal,
-    val acknowledgedQuantity: BigDecimal
+    val totalQuantity: TotalQuantity,
+    val openQuantity: OpenQuantity,
+    val executedQuantity: ExecutedQuantity,
+    val workedQuantity: WorkedQuantity,
+    val cancelledQuantity: CancelledQuantity
 ) {
-    constructor(totalQuantity: BigDecimal): this(totalQuantity, totalQuantity, BigDecimal.ZERO, BigDecimal.ZERO)
+    constructor(totalQuantity: TotalQuantity): this(totalQuantity, totalQuantity, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO)
 
     init {
         validate()
     }
 
     private fun validate() {
-        check(totalQuantity >= openQuantity + executedQuantity + acknowledgedQuantity) { "Total Quantity [$totalQuantity] must be more than the total of Executed [$executedQuantity], Open [$openQuantity] and Acknowledged [$acknowledgedQuantity] quantities = [${openQuantity + executedQuantity + acknowledgedQuantity}]" }
+        check(totalQuantity >= openQuantity + executedQuantity + workedQuantity + cancelledQuantity) { "Total Quantity [$totalQuantity] must be more than the total of Executed [$executedQuantity], Open [$openQuantity] and Acknowledged [$workedQuantity] and Cancelled [$cancelledQuantity] quantities = [${openQuantity + executedQuantity + workedQuantity + cancelledQuantity}]" }
     }
 }
+
+typealias TotalQuantity = BigDecimal
+typealias OpenQuantity = BigDecimal
+typealias ExecutedQuantity = BigDecimal
+typealias WorkedQuantity = BigDecimal
+typealias CancelledQuantity = BigDecimal
