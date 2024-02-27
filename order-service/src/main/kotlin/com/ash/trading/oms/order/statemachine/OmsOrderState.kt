faee81ea3c3e5bb34e1311731d6dc4de1e18a7f5
Ash French <ashfrench@users.noreply.github.com>
2024-02-27 22:21:08 +0000

    tidy up states

--- order-service/src/main/kotlin/com/ash/trading/oms/order/statemachine/OmsOrderState.kt
+++ order-service/src/main/kotlin/com/ash/trading/oms/order/statemachine/OmsOrderState.kt
@@ -4,22 +4,24 @@ import com.ash.trading.oms.model.OrderQuantity
 import com.ash.trading.oms.order.statemachine.events.OmsOrderEvent
 import com.ash.trading.oms.order.statemachine.handlers.*
 
-enum class OmsOrderState: IOmsOrderState {
+enum class OmsOrderState {
 
 
     NEW {
-        override fun <T> handleEvent(data: OrderQuantity, event: OmsOrderEvent<T>): Pair<OrderQuantity, IOmsOrderState> = OmsOrderNewStateHandler.handleEvent(data, event)
+        override fun <T> handleEvent(data: OrderQuantity, event: OmsOrderEvent<T>): Pair<OrderQuantity, OmsOrderState> = OmsOrderNewStateHandler.handleEvent(data, event)
     },
     WORKED {
-        override fun <T> handleEvent(data: OrderQuantity, event: OmsOrderEvent<T>): Pair<OrderQuantity, IOmsOrderState> = OmsOrderWorkedStateHandler.handleEvent(data, event)
+        override fun <T> handleEvent(data: OrderQuantity, event: OmsOrderEvent<T>): Pair<OrderQuantity, OmsOrderState> = OmsOrderWorkedStateHandler.handleEvent(data, event)
     },
     EXECUTED {
-        override fun <T> handleEvent(data: OrderQuantity, event: OmsOrderEvent<T>): Pair<OrderQuantity, IOmsOrderState> = OmsOrderExecutedStateHandler.handleEvent(data, event)
+        override fun <T> handleEvent(data: OrderQuantity, event: OmsOrderEvent<T>): Pair<OrderQuantity, OmsOrderState> = OmsOrderExecutedStateHandler.handleEvent(data, event)
     },
     PARTIALLY_EXECUTED {
-        override fun <T> handleEvent(data: OrderQuantity, event: OmsOrderEvent<T>): Pair<OrderQuantity, IOmsOrderState> = OmsOrderPartiallyExecutedStateHandler.handleEvent(data, event)
+        override fun <T> handleEvent(data: OrderQuantity, event: OmsOrderEvent<T>): Pair<OrderQuantity, OmsOrderState> = OmsOrderPartiallyExecutedStateHandler.handleEvent(data, event)
     },
     CANCELLED {
-        override fun <T> handleEvent(data: OrderQuantity, event: OmsOrderEvent<T>): Pair<OrderQuantity, IOmsOrderState> = OmsOrderCancelledStateHandler.handleEvent(data, event)
-    },
+        override fun <T> handleEvent(data: OrderQuantity, event: OmsOrderEvent<T>): Pair<OrderQuantity, OmsOrderState> = OmsOrderCancelledStateHandler.handleEvent(data, event)
+    };
+
+    abstract fun <T> handleEvent(data: OrderQuantity, event: OmsOrderEvent<T>): Pair<OrderQuantity, Any>
 }
