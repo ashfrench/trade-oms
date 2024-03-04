ad8c8c6abf062fcf8761a5db09003c2e004ed55f
Ash French <ash.french1@gmail.com>
2024-03-04 11:33:42 +0000

    validation for TradeOrderQuantities

--- data-model/src/main/kotlin/com/ash/trading/oms/model/TradeOrderQuantities.kt
+++ data-model/src/main/kotlin/com/ash/trading/oms/model/TradeOrderQuantities.kt
@@ -11,4 +11,20 @@ data class TradeOrderQuantities(
     val executedQuantity: ExecutedQuantity = tradeQuantities.values.sumOf { it }
     val usedQuantity: UsedQuantity = executedQuantity + cancelledQuantity
     val openQuantity: OpenQuantity = totalQuantity - usedQuantity
+
+    init {
+        validate()
+    }
+
+    private fun validate() {
+        check(orderQuantities.all { it.value > BigDecimal.ZERO }) { "All Order Quantities must be greater than 0"}
+        check(tradeQuantities.all { it.value > BigDecimal.ZERO }) { "All Trade Quantities must be greater than 0"}
+        check(cancelledQuantity >= BigDecimal.ZERO) { "Cancelled Quantity [$cancelledQuantity] must be greater than or equal to 0"}
+
+        check(totalQuantity >= executedQuantity) { "Total Quantity [$totalQuantity] must be greater than or equal to Executed Quantity [$executedQuantity]"}
+        check(totalQuantity >= cancelledQuantity) { "Total Quantity [$totalQuantity] must be greater than or equal to Cancelled Quantity [$cancelledQuantity]"}
+
+        check(totalQuantity >= executedQuantity + cancelledQuantity) { "Total Quantity [$totalQuantity] must be greater than the total of Executed [$executedQuantity] and Cancelled [$cancelledQuantity] quantities = [${executedQuantity + cancelledQuantity}]" }
+    }
+
 }
