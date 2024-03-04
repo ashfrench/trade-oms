5b4fa6d8f4ea4788d45b275f085492fb06b87325
Ash French <ash.french1@gmail.com>
2024-03-04 11:39:06 +0000

    add start of tests

---
+++ data-model/src/test/kotlin/com/ash/trading/oms/model/TradeOrderQuantitiesTest.kt
@@ -0,0 +1,30 @@
+package com.ash.trading.oms.model
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.math.BigDecimal
+
+class TradeOrderQuantitiesTest {
+
+    @Test
+    fun `basic quantity is valid`() {
+        val orderQuantity = TradeOrderQuantities(mapOf(newOrderId() to BigDecimal.ONE))
+        assertAll(
+            { assertEquals(orderQuantity.totalQuantity, BigDecimal.ONE) { "Total Quantity should be equal 1" } },
+            { assertEquals(orderQuantity.openQuantity, BigDecimal.ONE) { "Open Quantity should be equal 1" } },
+            { assertEquals(orderQuantity.executedQuantity, BigDecimal.ZERO) { "Executed Quantity should be equal 0" } },
+            { assertEquals(orderQuantity.cancelledQuantity, BigDecimal.ZERO){ "Cancelled Quantity should be equal 0" } },
+            { assertEquals(orderQuantity.usedQuantity, BigDecimal.ZERO) { "Used Quantity should be equal 0" } }
+        )
+    }
+
+    @Test
+    fun `invalid order quantity`() {
+        val exception = assertThrows<IllegalStateException>("All Order Quantities must be greater than 0") {
+            TradeOrderQuantities(mapOf(newOrderId() to BigDecimal(-1)))
+        }
+        assertEquals("All Order Quantities must be greater than 0", exception.message)
+    }
+
+}
