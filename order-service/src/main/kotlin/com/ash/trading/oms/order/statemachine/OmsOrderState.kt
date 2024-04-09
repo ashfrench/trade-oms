4c679cac49e3d675e26e7f97eb69f849784123e2
Ash French <ash.french1@gmail.com>
2024-04-09 08:22:42 +0100

    helper functions

--- order-service/src/main/kotlin/com/ash/trading/oms/order/statemachine/OmsOrderState.kt
+++ order-service/src/main/kotlin/com/ash/trading/oms/order/statemachine/OmsOrderState.kt
@@ -11,33 +11,35 @@ enum class OmsOrderState {
 
     NEW {
         override fun handleEvent(data: OrderQuantity, event: OmsOrderEvent): OrderQuantityState = OmsOrderNewStateHandler.handleEvent(data, event)
-        override fun isValid(data: OrderQuantity): Boolean = data.usedQuantity == BigDecimal.ZERO
+        override fun isValid(data: OrderQuantity) = data.usedQuantity == BigDecimal.ZERO
 
     },
     WORKED {
         override fun handleEvent(data: OrderQuantity, event: OmsOrderEvent): OrderQuantityState = OmsOrderWorkedStateHandler.handleEvent(data, event)
-        override fun isValid(data: OrderQuantity): Boolean =
+        override fun isValid(data: OrderQuantity) =
             data.workedQuantity > BigDecimal.ZERO && data.executedQuantity == BigDecimal.ZERO && data.cancelledQuantity.quantity == BigDecimal.ZERO
     },
     EXECUTED {
         override fun handleEvent(data: OrderQuantity, event: OmsOrderEvent): OrderQuantityState = OmsOrderExecutedStateHandler.handleEvent(data, event)
-        override fun isValid(data: OrderQuantity): Boolean = data.executedQuantity == data.totalQuantity
+        override fun isValid(data: OrderQuantity) = data.executedQuantity == data.totalQuantity
     },
     PARTIALLY_EXECUTED {
         override fun handleEvent(data: OrderQuantity, event: OmsOrderEvent): OrderQuantityState = OmsOrderPartiallyExecutedStateHandler.handleEvent(data, event)
-        override fun isValid(data: OrderQuantity): Boolean =
+        override fun isValid(data: OrderQuantity) =
             data.executedQuantity > BigDecimal.ZERO && data.executedQuantity < data.totalQuantity && data.cancelledQuantity.quantity == BigDecimal.ZERO
     },
     CANCELLED {
         override fun handleEvent(data: OrderQuantity, event: OmsOrderEvent): OrderQuantityState = OmsOrderCancelledStateHandler.handleEvent(data, event)
-        override fun isValid(data: OrderQuantity): Boolean =
+        override fun isValid(data: OrderQuantity) =
             data.cancelledQuantity > BigDecimal.ZERO && data.openQuantity == BigDecimal.ZERO
     };
 
     abstract fun handleEvent(data: OrderQuantity, event: OmsOrderEvent): OrderQuantityState
     abstract fun isValid(data: OrderQuantity): Boolean
+
+    fun getState(data: OrderQuantity): OmsOrderState = entries.first { it.isValid(data) }
 }
 
-operator fun OmsOrderState.contains(data: OrderQuantity): Boolean = isValid(data)
+operator fun OmsOrderState.contains(data: OrderQuantity) = isValid(data)
 
 typealias OrderQuantityState = Pair<OrderQuantity, OmsOrderState>
