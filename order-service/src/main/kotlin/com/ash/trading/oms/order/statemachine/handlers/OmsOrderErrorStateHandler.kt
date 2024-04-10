b2346d642740c92c7275737cb2f083e314c244e7
Ash French <ash.french1@gmail.com>
2024-04-10 16:12:28 +0100

    better order state handling

--- order-service/src/main/kotlin/com/ash/trading/oms/order/statemachine/handlers/OmsOrderErrorStateHandler.kt
+++ order-service/src/main/kotlin/com/ash/trading/oms/order/statemachine/handlers/OmsOrderErrorStateHandler.kt
@@ -1,14 +1,12 @@
 package com.ash.trading.oms.order.statemachine.handlers
 
 import com.ash.trading.oms.model.OrderQuantity
-import com.ash.trading.oms.model.compareTo
 import com.ash.trading.oms.order.statemachine.OmsOrderState
 import com.ash.trading.oms.order.statemachine.OrderQuantityState
 import com.ash.trading.oms.order.statemachine.contains
 import com.ash.trading.oms.order.statemachine.events.AdminFixEvent
 import com.ash.trading.oms.order.statemachine.events.OmsOrderEvent
 import org.slf4j.LoggerFactory
-import java.math.BigDecimal
 
 object OmsOrderErrorStateHandler {
 
@@ -20,18 +18,35 @@ object OmsOrderErrorStateHandler {
             "${OmsOrderState.ERROR} data should not be valid for another state $validState"
         }
 
-        return if (event is AdminFixEvent) {
-            logger.info("User ${event.userId} reset with data ${event.data} to state ${event.newState}")
-            if (event.newState.isValid(event.data)) {
-                event.data to event.newState
-            } else {
-                logger.warn("Updated Data ${event.data} was invalid for new state ${event.newState}")
-                data to OmsOrderState.ERROR
+        return try {
+            when (event) {
+                is AdminFixEvent -> handleAdminFixEvent(data, event)
+                else -> handleUnplannedEvent(data, event)
             }
-        } else {
-            logger.warn("Invalid Event Type [${event.javaClass.simpleName}] from ${OmsOrderState.ERROR} state")
+        } catch (e: Exception) {
+            logger.error("Error Handling Event Type [${event.javaClass.simpleName}] from ${OmsOrderState.ERROR} state", e)
             data to OmsOrderState.ERROR
         }
+    }
+
+    private fun handleAdminFixEvent(
+        data: OrderQuantity,
+        event: AdminFixEvent
+    ): OrderQuantityState {
+        val (userId, newData, newState) = event
+
+        logger.info("User $userId reset with data $newData to state $newState")
+        return if (newState.isValid(newData)) {
+            newData to newState
+        } else {
+            logger.warn("Updated Data ${event.data} was invalid for new state ${event.newState}")
+            data to OmsOrderState.ERROR
+        }
+    }
+
+    private fun handleUnplannedEvent(data: OrderQuantity, event: OmsOrderEvent): OrderQuantityState {
+        logger.warn("Invalid Event Type [${event.javaClass.simpleName}] from ${OmsOrderState.ERROR} state")
+        return data to OmsOrderState.ERROR
     }
 
 }
