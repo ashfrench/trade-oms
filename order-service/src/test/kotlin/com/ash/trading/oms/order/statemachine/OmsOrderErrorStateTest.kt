4c2726902daf2d68a2aaf97c71d18988ff7351ea
Ash French <ash.french1@gmail.com>
2024-04-11 08:05:13 +0100

    better tests for error state

--- order-service/src/test/kotlin/com/ash/trading/oms/order/statemachine/OmsOrderErrorStateTest.kt
+++ order-service/src/test/kotlin/com/ash/trading/oms/order/statemachine/OmsOrderErrorStateTest.kt
@@ -5,7 +5,6 @@ import com.ash.trading.oms.model.OrderQuantity
 import com.ash.trading.oms.model.newTradeId
 import com.ash.trading.oms.model.newUserId
 import com.ash.trading.oms.order.statemachine.events.AdminFixEvent
-import com.ash.trading.oms.order.statemachine.events.OrderCancelledEvent
 import com.ash.trading.oms.order.statemachine.events.TraderExecutedEvent
 import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -54,6 +53,61 @@ class OmsOrderErrorStateTest {
             { assertEquals(orderQuantity, updatedOrderQuantity) },
             { assertEquals(OmsOrderState.ERROR, updatedState) }
         )
+    }
+
+    @Test
+    fun `error state when has valid new data`() {
+        val orderQuantity = OrderQuantity(BigDecimal.TEN)
+        val exception = assertThrows<IllegalStateException> { OmsOrderState.ERROR.handleEvent(
+            orderQuantity,
+            TraderExecutedEvent(newTradeId(), BigDecimal.TEN)
+        ) }
+
+        assertEquals("ERROR data should not be valid for another state NEW", exception.message)
+    }
+
+    @Test
+    fun `error state when has valid worked data`() {
+        val orderQuantity = OrderQuantity(BigDecimal.TEN, workedQuantity = BigDecimal.TEN)
+        val exception = assertThrows<IllegalStateException> { OmsOrderState.ERROR.handleEvent(
+            orderQuantity,
+            TraderExecutedEvent(newTradeId(), BigDecimal.TEN)
+        ) }
+
+        assertEquals("ERROR data should not be valid for another state WORKED", exception.message)
+    }
+
+    @Test
+    fun `error state when has valid partially executed data`() {
+        val orderQuantity = OrderQuantity(BigDecimal.TEN, workedQuantity = BigDecimal(8), executedQuantity = BigDecimal.TWO)
+        val exception = assertThrows<IllegalStateException> { OmsOrderState.ERROR.handleEvent(
+            orderQuantity,
+            TraderExecutedEvent(newTradeId(), BigDecimal.TEN)
+        ) }
+
+        assertEquals("ERROR data should not be valid for another state PARTIALLY_EXECUTED", exception.message)
+    }
+
+    @Test
+    fun `error state when has valid executed data`() {
+        val orderQuantity = OrderQuantity(BigDecimal.TEN, executedQuantity = BigDecimal.TEN)
+        val exception = assertThrows<IllegalStateException> { OmsOrderState.ERROR.handleEvent(
+            orderQuantity,
+            TraderExecutedEvent(newTradeId(), BigDecimal.TEN)
+        ) }
+
+        assertEquals("ERROR data should not be valid for another state EXECUTED", exception.message)
+    }
+
+    @Test
+    fun `error state when has valid cancelled data`() {
+        val orderQuantity = OrderQuantity(BigDecimal.TEN, cancelledQuantity = CancelledQuantity(BigDecimal.TEN, LocalDateTime.now()))
+        val exception = assertThrows<IllegalStateException> { OmsOrderState.ERROR.handleEvent(
+            orderQuantity,
+            TraderExecutedEvent(newTradeId(), BigDecimal.TEN)
+        ) }
+
+        assertEquals("ERROR data should not be valid for another state CANCELLED", exception.message)
     }
 
 }
