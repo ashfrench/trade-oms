668efd932fed5946ac34bac308a47831cb419350
Ash French <ash.french1@gmail.com>
2024-04-21 11:01:13 +0100

    better validation

--- trade-order-service/src/main/kotlin/com/ash/trading/oms/tradeorder/statemachine/handlers/OmsTradeOrderTerminalStateHandler.kt
+++ trade-order-service/src/main/kotlin/com/ash/trading/oms/tradeorder/statemachine/handlers/OmsTradeOrderTerminalStateHandler.kt
@@ -3,6 +3,7 @@ package com.ash.trading.oms.tradeorder.statemachine.handlers
 import com.ash.trading.oms.model.TradeOrderQuantities
 import com.ash.trading.oms.tradeorder.statemachine.OmsTradeOrderState
 import com.ash.trading.oms.tradeorder.statemachine.TradeOrderQuantitiesState
+import com.ash.trading.oms.tradeorder.statemachine.contains
 import com.ash.trading.oms.tradeorder.statemachine.event.OmsTradeOrderEvent
 import org.slf4j.LoggerFactory
 
@@ -11,6 +12,8 @@ class OmsTradeOrderTerminalStateHandler(private val state: OmsTradeOrderState) {
     private val logger = LoggerFactory.getLogger(OmsTradeOrderTerminalStateHandler::class.java)
 
     fun handleEvent(data: TradeOrderQuantities, event: OmsTradeOrderEvent): TradeOrderQuantitiesState {
+        check(data in state)
+
         logger.warn("Invalid Event Type [${event.javaClass.simpleName}] from [$state] state")
         return data to state
     }
